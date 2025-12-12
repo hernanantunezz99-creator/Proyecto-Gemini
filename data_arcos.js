fba6a50968e2f2cc0e86a0ef4acd77ecd8f93371
hernanantunezz99-creator <hernan.antunezz99@gmail.com>
2025-12-12 04:29:07 -0300

    Update and rename data_arcos.js.js to data_arcos.js

---
+++ data_arcos.js
@@ -0,0 +1,12 @@
+const DATOS_ARCOS = {
+    1: [
+        { titulo: "ARCO 1", sub: "Enero - Abril", icono: "🧱" },
+        { titulo: "ARCO 2", sub: "Mayo - Agosto", icono: "🌱" },
+        { titulo: "ARCO 3", sub: "Sept - Dic", icono: "🚀" }
+    ],
+    2: [
+        { titulo: "ARCO 1", sub: "Enero - Abril", icono: "🏗️" },
+        { titulo: "ARCO 2", sub: "Mayo - Agosto", icono: "📈" },
+        { titulo: "ARCO 3", sub: "Sept - Dic", icono: "🎓" }
+    ]
+};
